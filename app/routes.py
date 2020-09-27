04050e46dd909dc610feeaae8ee2965cb1a3391c
langat justus <justuslangat78@gmail.com>
2020-09-27 12:05:09 +0300

    fixed a bug

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, requests, db, bcrypt
 from flask import render_template, url_for, flash, redirect, request,abort
-from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm,PostForm,CommentForm
-from blog.models import User, Post, Comment
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm,PostForm,CommentForm
+from app.models import User, Post, Comment
 from flask_login import login_user, current_user, logout_user, login_required
 
 
