cb740eab636bd980f2fad3350b84feddaa8afbc0
langat justus <justuslangat78@gmail.com>
2020-09-27 12:03:53 +0300

    fixed a bug

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
-from app import app, requests
+from app import app, requests, db, bcrypt
+from flask import render_template, url_for, flash, redirect, request,abort
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm,PostForm,CommentForm
+from blog.models import User, Post, Comment
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
