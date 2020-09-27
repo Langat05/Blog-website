acd8ae0c82f0b7c76b2a7603dda6568b7a3ebb57
langat justus <justuslangat78@gmail.com>
2020-09-27 10:15:15 +0300

    added an update account form clASS

--- app/forms.py
+++ app/forms.py
@@ -34,3 +34,23 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already in use')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email taken')
+
+
