b3b73e23922d666e3e8fa17d9982cb67f61fcdc7
langat justus <justuslangat78@gmail.com>
2020-09-27 10:28:25 +0300

    updated post database table and relationship

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from blog.models import User
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
-from app import db, login_manager
 from flask_login import UserMixin
+from datetime import datetime
+from app import db, login_manager
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -15,6 +18,26 @@ class User(db.Model,UserMixin):
     def __repr__(self):
             return f"User('{self.username}', '{self.email}')"
 
+class Post(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(100),nullable=False)
+    date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    content=db.Column(db.Text,nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+    
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.date_posted}')"
+    
 class Quote:
     def __init__ (self,author,quote):
         self.author = author
